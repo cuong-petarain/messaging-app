f0adf0a228a175e6fa309e91173d9a980d8f609d
Cuong Tran <cuonghtran@petarainsoft.com>
2024-06-20 14:11:41 +0700

    fix: improve UI of messages

--- Assets/Scripts/MessageCard.cs
+++ Assets/Scripts/MessageCard.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -19,26 +20,58 @@ public class MessageCard : MonoBehaviour
 
     public void Populate(string displayName, string messageContent, DateTime createTime, bool isThisUser = false)
     {
-        _textDisplayName.text = displayName + ":";
-        _textMessageContent.text = messageContent;
+        //_textDisplayName.text = displayName + ":";  // commented because conversations between 2 don't need names
+        _textMessageContent.text = InsertEveryXChars(messageContent, "<br>", 80);
         createDateTime = createTime;
-        _textSentTime.text = $"[{createTime.ToShortTimeString()}]";
+        _textSentTime.text = $"{createTime.ToShortTimeString()}";
         if (isThisUser)
         {
             _backgroundImage.color = _thisUserBackgroundColor;
             _horizontalLayoutGroup.childAlignment = TextAnchor.UpperRight;
+            _textSentTime.transform.SetSiblingIndex(0);
         }
         else
         {
             _backgroundImage.color = _otherUserBackgroundColor;
             _horizontalLayoutGroup.childAlignment = TextAnchor.UpperLeft;
+            _textSentTime.transform.SetSiblingIndex(1);
         }
     }
 
     public void PopulateDateGroup(DateTime createTime)
     {
-        _textSentTime.text = createTime.ToString("MMMM d, yyyy");
-        _textSentTime.fontSize += 2;
-        _textSentTime.color = Color.black;
+        _textMessageContent.text = createTime.ToString("MMMM d, yyyy");
+        _textMessageContent.fontSize += 2;
+        _textSentTime.gameObject.SetActive(false);
+    }
+
+    private string InsertEveryXChars(string original, string insertion, int interval)
+    {
+        if (interval <= 0)
+        {
+            throw new ArgumentException("Interval must be greater than zero.");
+        }
+
+        if (string.IsNullOrEmpty(original) || string.IsNullOrEmpty(insertion))
+        {
+            return original;
+        }
+
+        StringBuilder modifiedString = new StringBuilder();
+
+        for (int i = 0; i < original.Length; i++)
+        {
+            modifiedString.Append(original[i]);
+            if ((i + 1) % interval == 0 && i != original.Length - 1)
+            {
+                modifiedString.Append(insertion);
+
+                Vector2 sizeDelta = transform.GetComponent<RectTransform>().sizeDelta;
+                sizeDelta.y += 30;
+                transform.GetComponent<RectTransform>().sizeDelta = sizeDelta;
+            }
+        }
+
+        return modifiedString.ToString();
     }
 }
